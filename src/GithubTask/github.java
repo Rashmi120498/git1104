b6919daaf1bf4aae95478a78036fb4ce0727781c
Rashmi120498 <ammurashmi22@gmail.com>
2022-04-11 17:16:35 +0530

    My Git Task

---
+++ src/GithubTask/github.java
@@ -0,0 +1,25 @@
+package GithubTask;
+public class github {
+	
+public void EmployeeDetails() {
+		
+		int empId = 1234567678;
+		String empName = "rashmi";
+		long empPhoneno = 12343456678l;
+		float empsalary = 25000.34564f;
+		char empGender = 'f';
+		String empCity = "viluppuram";
+		String empEmail = "sairudhu12@gmail.com";
+		
+		System.out.println("Employee Id is :"+ empId);
+		System.out.println("Employee name is :"+ empName);
+		System.out.println("Employee phoneno is :"+ empPhoneno);
+		System.out.println("Employee salary is :"+ empsalary);
+		System.out.println("Employee gender is :"+ empGender);
+		System.out.println("Employee city is :"+ empCity);
+		System.out.println("Employee email is :"+ empEmail);
+	}
+	
+	
+
+}
